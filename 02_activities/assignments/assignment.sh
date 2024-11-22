478f5d969adb66ea6450eed5cd1a64e77fc20cb1
baxik88 <baxwork88@gmail.com>
2024-11-22 12:52:52 -0500

    doing errors

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,10 +14,8 @@ touch README.md
 touch analysis/main.py
 
 # download client data
-wget -O rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
 unzip rawdata.zip
-
-
 
 ###########################################
 # Complete assignment here
